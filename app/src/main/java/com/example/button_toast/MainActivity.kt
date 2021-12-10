f8ffe36e3fb8dce49389b7ec358c696c4dae3167
DrThundercat <tim35plit3r@gmail.com>
2021-12-09 21:21:55 -0600

    inital commit

---
+++ app/src/main/java/com/example/button_toast/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.button_toast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // storing ID of the button
+        // in a variable
+        val button = findViewById<Button>(R.id.button)
+
+        // operations to be performed
+        // when user tap on the button
+        button?.setOnClickListener()
+        {
+            // displaying a toast message
+            Toast.makeText(this@MainActivity, R.string.message, Toast.LENGTH_LONG).show()
+        }
+    }
+}
